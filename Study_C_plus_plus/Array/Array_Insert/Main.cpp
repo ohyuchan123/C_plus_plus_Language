71b678cebe0ec12b9e9c1e5777e7714c04c731a7
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2023-11-11 13:14:44 +0900

    Array Insert

--- Study_C_plus_plus/Array/Array_Insert/Main.cpp
+++ Study_C_plus_plus/Array/Array_Insert/Main.cpp
@@ -8,7 +8,17 @@ using namespace std;
 // 배열의 단점
 // 배열은 요소를 추가/삭제하는 것이 어렵다!
 
-void Insert(int** pArr, int length, int index, int value);
+// 자료구조
+// 배열 : 기본적인 형태의 자료구조
+// 배열 : 여러 개의 저장 공간이 일자로 나열되어 있는 구조
+
+// 템플릿 template
+// 클래스나 함수를 찍어내는 툴
+// 데이터 형식을 일반화
+
+template<typename T>
+// void Insert(int** pArr, int length, int index, int value);
+void Insert(T** pArr, int length, int index, T value);
 
 int main(){
     int* arr = new int[5];
@@ -35,10 +45,29 @@ int main(){
     delete[] arr; // 동적 할당 해제
 }
 
-void Insert(int **pArr, int length, int index, int value)
+// void Insert(int **pArr, int length, int index, int value)
+// {
+//     int* tempArr = *pArr;
+//     int* newArr = new int[++length];
+//     for(int i=0; i<length; i++){
+//         if(i < index){
+//             newArr[i] = tempArr[i];
+//         }else if(i == index){
+//             newArr[i] = value;
+//         }else{
+//             newArr[i] = tempArr[i - 1];
+//         }
+//     }
+    
+//     delete[] tempArr;
+//     *pArr = newArr;
+// }
+
+template<typename T>
+void Insert(T **pArr, int length, int index, T value)
 {
-    int* tempArr = *pArr;
-    int* newArr = new int[++length];
+    T* tempArr = *pArr;
+    T* newArr = new int[++length];
     for(int i=0; i<length; i++){
         if(i < index){
             newArr[i] = tempArr[i];
